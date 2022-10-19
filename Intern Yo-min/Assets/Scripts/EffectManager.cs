a077ab005c84b6fd6d021f4341314d7acd449710
TAITAI-FRESHOSR\takat <takatai2020game@gmail.com>
2022-10-19 15:00:28 +0900

    エフェクトの機能追加

---
+++ Intern Yo-min/Assets/Scripts/EffectManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+
+public class EffectManager : SingletonMonoBehaviour<EffectManager>
+{
+    [System.Serializable]
+    public struct Effect_Info
+    {
+        public string EffectName;
+        public GameObject EffectObj;
+    }
+
+    [SerializeField] private Effect_Info[] EffectObj;
+
+    private void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject); // ÉVÅ[ÉìÇ™ïœÇÌÇ¡ÇƒÇ‡éÄÇ»Ç»Ç¢
+    }
+
+
+    public void SetEffect(string EffectName, Vector3 Pos, Quaternion Rot)
+    {
+        for (int i = 0; i < EffectObj.Length; ++i)
+        {
+            if (EffectObj[i].EffectName == EffectName)
+            {
+                GameObject Effect = Instantiate(EffectObj[i].EffectObj, Pos, Rot);
+                ParticleSystem[] particles;
+                particles = Effect.gameObject.GetComponentsInChildren<ParticleSystem>();
+
+                for (int j = 0; j < particles.Length; ++j)
+                {
+                    particles[i].Play();
+                }
+                return;
+            }
+        }
+    }
+}
