efeed6216fbe974dcecf7438a9050a4bad7854ea
TAITAI-FRESHOSR\takat <takatai2020game@gmail.com>
2022-10-27 16:32:21 +0900

    コマ設置音追加

--- Intern Yo-min/Assets/Scripts/Game/Masu.cs
+++ Intern Yo-min/Assets/Scripts/Game/Masu.cs
@@ -42,6 +42,7 @@ public class Masu : MonoBehaviour
 
         // 設置エフェクト
         EffectManager.Instance.SetEffect("Test", transform.position + new Vector3(0, GamePlayManager.MasuScaleY + OseroObj.transform.localScale.y, 0), Quaternion.identity, 2.0f);
+        SoundManager.Instance.PlaySound("コマ設置", false);
 
         // オセロをひっくり返す処理
         GamePlayManager.PlayerOseroTypeInfo MyOseroType = OseroObj.GetOseroType();
